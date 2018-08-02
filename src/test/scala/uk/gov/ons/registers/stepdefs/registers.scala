4ba2b1c2638bbf3c94ffecd95db5378fbce4df03
haqa <ameenhaq79@gmail.com>
2018-08-02 12:54:30 +0100

    Fixed sad path - passing invalid path for properties or input data file

--- src/test/scala/uk/gov/ons/registers/stepdefs/registers.scala
+++ src/test/scala/uk/gov/ons/registers/stepdefs/registers.scala
@@ -9,7 +9,7 @@ import uk.gov.ons.registers.helpers.CSVProcessor.FilePath
 // TODO - move to registers package
 package object stepdefs {
   var inputPath: FilePath = _
-  var outputPath: FilePath = _
+  var outputPath: Path = _
   var stratificationPropertiesPath: FilePath = _
   var outputDataDF: DataFrame = _
 
