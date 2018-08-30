b54fc394f7d8157babf9ba8561aa2bc1efbd7c3c
haqa <ameenhaq79@gmail.com>
2018-08-30 23:22:47 +0100

    Added fold, map2, map3 ... functions for Validation type

--- src/main/scala/uk/gov/ons/registers/Validation.scala
+++ src/main/scala/uk/gov/ons/registers/Validation.scala
@@ -9,9 +9,17 @@ case class Failure[E](head: E, tail: List[E] = Nil) extends Validation[E, Nothin
 object Validation {
   type ErrorMessage = String
 
+  private def apply[A, B, E](vFn: Validation[E, A => B])(vA: Validation[E, A]): Validation[E, B] =
+    map2(vFn, vA) {(f, a) =>
+      f(a)
+    }
+
+  private def unit[E, A](a: => A): Validation[E, A] =
+    Success(a)
+
   def toOption[A, B, C, E](va: Validation[E, A], vb: Validation[E, B])
     (onFailure: List[E] => Option[C], onSuccess: (A, B) => Option[C]): Option[C] = (va, vb) match {
-    case (Success(validA), Success(validB)) => onSuccess(validA, validB)
+    case (Success(a), Success(b)) => onSuccess(a, b)
     case (Failure(ha, ta), Failure(hb, tb)) => onFailure(ha +: ta ++: hb +: tb)
     case (Failure(h, t), _) => onFailure(h +: t)
     case (_, Failure(h, t)) => onFailure(h +: t)
@@ -20,6 +28,26 @@ object Validation {
   def toValidation[A](aTry: Try[A]): Validation[ErrorMessage, A] =
     aTry match {
       case util.Success(v) => Success(v)
-      case util.Failure(ex) => Failure(ex.getMessage)
+      case util.Failure(e) => Failure(e.getMessage)
     }
+
+  def fold[A, B, E](v: Validation[E, A])(onFailure: List[E] => B, onSuccess: A => B): B =
+    v match{
+      case Success(s) => onSuccess(s)
+      case Failure(h, t) => onFailure(h +: t)
+    }
+
+  def map2[A, B, C, E](va: Validation[E, A], vb: Validation[E, B])(f: (A, B) => C): Validation[E, C] =
+    (va, vb) match {
+      case (Success(a), Success(b)) => Success(f(a, b))
+      case (Failure(ha, ta), Failure(hb, tb)) => Failure(ha, ta ++: hb +: tb)
+      case (Failure(h, t), _) => Failure(h, t)
+      case (_, Failure(h, t)) => Failure(h, t)
+    }
+
+  def map3[A, B, C, D, E](va: Validation[E, A], vb: Validation[E, B], vc: Validation[E, C])(f: (A, B, C) => D): Validation[E, D] = {
+    val vbcd = apply(unit(f.curried))(va)
+    val vcd = apply(vbcd)(vb)
+    apply(vcd)(vc)
+  }
 }
