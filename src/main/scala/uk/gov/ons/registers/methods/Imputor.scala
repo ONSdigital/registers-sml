5d4e1a6a08d1b95281259f8debe6a07935bd2ca0
Vlad Shiligin <vlad.shiligin@ons.gov.uk>
2018-10-17 15:22:17 +0100

    Reg 1920 (#22)
    * imputing turnover and employees added
    
    * imputing turnover and employees added
    
    * imputing turnover and employees added
    
    * imputing turnover and employees added
    
    * imputing turnover and employees added
    
    * imputing turnover and employees added
    
    * imputing turnover and employees added

---
+++ src/main/scala/uk/gov/ons/registers/methods/Imputor.scala
@@ -0,0 +1,55 @@
+package uk.gov.ons.registers.methods
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import uk.gov.ons.spark.sql._
+
+trait Imputor {
+/**
+  * returns tuple representing imp_empees, imp_turnover
+  * */
+  def imputeEmployees(turnover:Option[String], payeEmployees:Option[String], tph:Option[String]):(String,String) = {
+    val inputs = (turnover,payeEmployees,tph)
+
+      try{
+        inputs match{
+        case (Some(trn),None,Some(tph)) => ((trn.toInt / tph.toInt).toString,null) //turnover != null, paye employees = null
+        case (None,Some(emps),Some(tph)) => (null,(emps.toInt / tph.toInt).toString)//paye employees!= null, turnover  = null
+        case (Some(trn),Some(emps),Some(tph)) => ((trn.toInt / tph.toInt).toString,(emps.toInt / tph.toInt).toString)//turnover != null, paye employees != null
+        case (None,None,_) => (null,null) ////turnover = null, paye employees = null
+        case (_,_,None) => throw new IllegalArgumentException("tph is null, returning tuple (imputed turnover = null, imputed employees = null)") // abnormal case: tph = null
+    }}catch{
+        case iae: IllegalArgumentException => (null,null) //error thrown for logging. Return imputed employees and imputed turnover set to nulls
+      }
+
+  }
+
+  val imputedSchema = new StructType()
+    .add(StructField("ern", StringType,false))
+    .add(StructField("imp_turnover", StringType,true))
+    .add(StructField("imp_empees", StringType,true))
+
+  /**
+    *
+    * */
+  def imputeTurnoverAndEmpees(df:DataFrame, tphDF:DataFrame)(implicit spark: SparkSession):DataFrame = {
+
+    val withTphDF: DataFrame = df.join(tphDF,Seq("sic07"), "left_outer")
+
+    val imputedDS:RDD[Row] = withTphDF.rdd.map(row => {
+
+      val (trn,emps) = imputeEmployees(row.getOption[String]("turnover"),row.getOption[String]("paye_empees"), row.getOption[String]("tph"))
+
+      new GenericRowWithSchema(Array(
+
+         row.getAs[String]("ern"),
+         trn,
+         emps
+
+    ),imputedSchema)})
+    spark.createDataFrame(imputedDS,imputedSchema)
+  }
+
+}
---
+++ src/main/scala/uk/gov/ons/spark/extensions/package.scala
@@ -0,0 +1,31 @@
+package uk.gov.ons.spark
+
+
+
+import org.apache.spark.sql.Row
+
+
+
+
+package object sql {
+
+
+  implicit class SqlRowExtensions(val row:Row){
+
+
+
+    def getOption[T](field:String):Option[T]= if(row.isNull(field)) None
+    else Option[T](row.getAs[T](field))
+
+    def isNull(field:String):Boolean = try {
+      row.isNullAt(row.fieldIndex(field))  //if field exists and the value is null
+    }catch {
+      case iae:IllegalArgumentException => true  //if field does not exist
+      case e: Throwable => {
+        println(s"field: ${if (field==null) "null" else field.toString()}")
+        throw e
+      }
+    }
+  }
+
+}
