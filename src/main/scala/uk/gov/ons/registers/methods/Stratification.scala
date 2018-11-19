c3868f71d0565aad5624764404e85048b32957d7
vladshiligin <vladtoldmetodoit@gmail.com>
2018-11-19 21:45:12 +0000

    stratification refactored, unit tests added, sampling and stratification performance issues addressed

--- src/main/scala/uk/gov/ons/registers/methods/Stratification.scala
+++ src/main/scala/uk/gov/ons/registers/methods/Stratification.scala
@@ -18,37 +18,17 @@ trait Stratification extends SmlLogger{
     val cellNo = StratificationPropertiesFields.cellNumber
     val unitDF = checkUnitForMandatoryFields(inputDf,bounds)
     val propsDF = StratificatonPropertyFieldsCasting.mapStartificationProperties(stratificationPropsDf)
-    val dataDf = unitDF.withColumn(cellNo, lit(-2).cast(IntegerType))
+    val dataDf = unitDF.withColumn(cellNo, when(col(bounds).isNull,-2).otherwise(-1).cast(IntegerType))
     dataDf.createOrReplaceTempView("datadf")
     propsDF.createOrReplaceTempView("propsdf")
-    val joinSql = """
-        SELECT datadf.ern,
-        datadf.entref,
-        datadf.name,
-        datadf.tradingstyle,
-        datadf.address1,
-        datadf.address2,
-        datadf.address3,
-        datadf.address4,
-        datadf.address5,
-        datadf.postcode,
-        datadf.legalstatus,
-        datadf.sic07,
-        datadf.paye_empees,
-        datadf.paye_jobs,
-        datadf.ent_turnover,
-        datadf.std_turnover,
-        datadf.grp_turnover,
-        datadf.cntd_turnover,
-        datadf.app_turnover,
-        datadf.prn,
-        CAST(datadf.cell_no AS INT),
+    val joinSql = s"""
+        SELECT datadf.*,
         CAST(propsdf.cell_no AS INT) AS allocated_cell_no
         FROM datadf
         LEFT JOIN propsdf ON datadf.sic07 >= propsdf.lower_class AND
                             sic07 <= CAST(propsdf.upper_class AS INT) AND
-                            datadf.paye_empees >= propsdf.lower_size AND
-                            datadf.paye_empees <= propsdf.upper_size
+                            datadf.$bounds >= propsdf.lower_size AND
+                            datadf.$bounds <= propsdf.upper_size
       """.stripMargin
 
     val joinedDF = spark.sql(joinSql)
