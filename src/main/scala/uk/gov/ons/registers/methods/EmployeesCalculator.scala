6065a8d702e4031369eda0f8dc89b1410405ec05
ranjit-das <Ranjit.Das@ons.gov.uk>
2018-12-18 13:15:56 +0000

    REG-2322: Common variable names used

--- src/main/scala/uk/gov/ons/registers/methods/EmployeesCalculator.scala
+++ src/main/scala/uk/gov/ons/registers/methods/EmployeesCalculator.scala
@@ -4,8 +4,8 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import uk.gov.ons.registers.model.CommonFrameDataFields._
 
 trait EmployeesCalculator {
-  val imputed = "imp_empees"
-  val employees = "empees"
+ // val imputed = "imp_empees"
+ // val employees = "empees"
 
   def calculateEmployees(empDF: DataFrame)(implicit activeSession: SparkSession): DataFrame = {
     val calculatedEmployeesDF = getGroupedByEmployment(empDF)
@@ -13,7 +13,7 @@ trait EmployeesCalculator {
   }
 
   def getGroupedByEmployment(empDF: DataFrame, empTableName: String = "EMPLOYEES")(implicit spark: SparkSession): DataFrame = {
-    val filEmpDF = empDF.select(ern, payeEmployees, imputed)
+    val filEmpDF = empDF.select(ern, payeEmployees, imp_empees)
     filEmpDF.createOrReplaceTempView(empTableName)
     val flatEmpDataSql = generateCalculateEmpSql(empTableName)
     spark.sql(flatEmpDataSql).select(ern, employees)
@@ -23,8 +23,8 @@ trait EmployeesCalculator {
     s"""
        SELECT $empTableName.*,
            CAST(
-          ((CASE WHEN $empTableName.$payeEmployees IS NULL AND $empTableName.$imputed IS NULL THEN 1
-                 WHEN $empTableName.$payeEmployees IS NULL THEN $empTableName.$imputed
+          ((CASE WHEN $empTableName.$payeEmployees IS NULL AND $empTableName.$imp_empees IS NULL THEN 1
+                 WHEN $empTableName.$payeEmployees IS NULL THEN $empTableName.$imp_empees
                  ELSE $empTableName.$payeEmployees END))
            AS INT) AS empees
            FROM $empTableName
